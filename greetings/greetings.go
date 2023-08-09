b63f12e0b0eb587edc832cf0e7c0f2ada9819d78
Heather Brillant <hbrillant@kyruus.com>
2023-08-09 14:32:36 -0400

    intro and modules

---
+++ greetings/greetings.go
@@ -0,0 +1,9 @@
+package greetings
+
+import "fmt"
+
+func Hello(name string) string {
+	// var message string is how to declare a variable. := is a shortcut to declare and assign
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message
+}
--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,19 @@
-package main
+package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func main() {
-    fmt.Println("Hello, World!")
+// Hello returns a greeting for the named person.
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// If a name was received, return a value that embeds the name
+	// in a greeting message.
+	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	return message, nil // (meaning no error) as a second value in the successful return. That way, the caller can see that the function succeeded.
 }
